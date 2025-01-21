a02399137ac0a09824c9a153a35f250e73439950
project_Html <khoanguyenduc99@gmail.com>
2025-01-21 08:51:42 +0700

    useState in React Native

--- App.tsx
+++ App.tsx
@@ -1,23 +1,43 @@
 
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState<string>("Khoa");
+
+  const [age, setAge] = useState<number>(21);
+
+  const [person, setPerson] = useState<{
+    name: string;
+    age: number;
+  }>({
+    name: "Khoa",
+    age: 23
+  })
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>Text</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
 
       </View>
       <Text>
         Hello world in React Native!
-        <Text>I'm Khoa</Text>
+        <Text style={styles.name}>I'm {name}</Text>
+        <Text style={styles.age}>I'm {age} years old</Text>
       </Text>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  age: {
+    fontSize: 20,
+    color: 'orange'
+  },
+  name: {
+    fontSize: 40,
+    color: 'blue'
+  },
   text: {
     color: 'green',
     fontSize: 40
@@ -25,7 +45,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 90,
+    paddingHorizontal: 40
   },
 });
